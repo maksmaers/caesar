21f770e9a15afc3dd75acf3595c43d42a2907679
Maks <maksim@red-dog.com>
2013-07-31 16:10:19 +0300

    Up

--- Caesar.php
+++ Caesar.php
@@ -20,39 +20,39 @@ class Caesar
 	static function getMessage($message, $mode = 'encode')
 	{
 		$result = '';
-        $amessage = str_split($message);
-        $count_symbs = strlen(self::$symbols);
+		$amessage = str_split($message);
+		$count_symbs = strlen(self::$symbols);
 
-        foreach ($amessage as $symbol)
-        {
-            if ($num = strpos(self::$symbols, $symbol))
-            {
-                if ($mode == 'encode')
-                {
-                    $num = $num + self::$key;
-                }
-                elseif ($mode == 'decode')
-                {
-                    $num = $num - self::$key;
-                }
+		foreach ($amessage as $symbol)
+		{
+			if ($num = strpos(self::$symbols, $symbol))
+			{
+				if ($mode == 'encode')
+				{
+					$num = $num + self::$key;
+				}
+				elseif ($mode == 'decode')
+				{
+					$num = $num - self::$key;
+				}
 
-                if ($num >= $count_symbs)
-                {
-                    $num = $num - $count_symbs;    
-                }
-                elseif  ($num < 0)
-                {
-                    $num = $num + $count_symbs;
-                }
+				if ($num >= $count_symbs)
+				{
+					$num = $num - $count_symbs;    
+				}
+				elseif  ($num < 0)
+				{
+					$num = $num + $count_symbs;
+				}
 
-                $result = $result . self::$symbols[$num];
-            }
-            else
-            {
-                $result = $result . $symbol;
-            }
-        }
+				$result = $result . self::$symbols[$num];
+			}
+			else
+			{
+				$result = $result . $symbol;
+			}
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 }
